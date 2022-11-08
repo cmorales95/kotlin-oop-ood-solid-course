ce1745ade917dbca461e0d0634c78773cfae4250
Cristian Morales <crisdavidmm95.dev@gmail.com>
2022-11-07 19:02:04 -0500

    add initialization to a Song class

--- src/main/kotlin/entities/Song.kt
+++ src/main/kotlin/entities/Song.kt
@@ -4,8 +4,13 @@ class Song(
     val title: String,
     val artist: String
 ) {
+    private val artistUpper = artist.uppercase()
+    init {
+       println("Song $title created")
+    }
+
     fun play() {
-        println("Playing Song: $title - $artist")
+        println("Playing Song: $title - $artistUpper")
     }
 
     fun stop() {
